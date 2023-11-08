9196a02c04fc3797def036188adf9d38acb54653
risqi2 <risqiikhsani12@gmail.com>
2023-11-08 11:39:35 +0700

    test

--- main.go
+++ main.go
@@ -70,6 +70,7 @@ func main() {
 	}
 
 	public := r.Group("/api")
+	public.Use(middlewares.AuthMiddleware())
 	public.Use(middlewares.LogMiddleware())
 	routes.SetupPublicAccountRoutes(public)
 	routes.SetupUserRoutes(public)

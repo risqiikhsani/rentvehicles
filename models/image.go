e8d5c5869e0e01dafdd25d751e2069b7e5181c79
risqi2 <risqiikhsani12@gmail.com>
2023-12-11 17:23:46 +0700

    tes

--- models/image.go
+++ models/image.go
@@ -34,7 +34,7 @@ func (i Image) GetClickableURL() string {
 	// Construct the full image URL by appending the path to the base URL
 	cleanPath := filepath.Clean(i.Path)
 	// return fmt.Sprintf("%s/%s/%s", baseURL, staticImagePath, filepath.Base(cleanPath))
-	return fmt.Sprintf("%s/%s", baseURL, filepath.Base(cleanPath))
+	return fmt.Sprintf("%s/%s/%s", baseURL, staticImagePath, filepath.Base(cleanPath))
 }
 
 // https://pkg.go.dev/encoding/json
